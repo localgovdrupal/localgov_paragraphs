78eb147a9de6490b247e73eaf4ca73fe9f3b62ea
Finn <finn@agile.coop>
2022-10-21 15:18:12 +0100

    Add more configSchemaCheckerExclusions..

--- modules/localgov_subsites_paragraphs/tests/src/Functional/SubsitesParagraphsAdministrationTest.php
+++ modules/localgov_subsites_paragraphs/tests/src/Functional/SubsitesParagraphsAdministrationTest.php
@@ -25,6 +25,8 @@ class SubsitesParagraphsAdministrationTest extends ParagraphsTestBase {
     'core.entity_form_display.paragraph.localgov_box_link.default',
     'core.entity_form_display.paragraph.localgov_featured_subsite.default',
     'core.entity_form_display.paragraph.localgov_key_contact_item.default',
+    'core.entity_form_display.paragraph.localgov_media_with_text.default',
+    'core.entity_form_display.paragraph.localgov_video.default',
   ];
 
   /**
