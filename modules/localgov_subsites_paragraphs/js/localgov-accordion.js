2339cd190f7877de499fbe459737786de6c1d908
Lee Mills <8024370+millnut@users.noreply.github.com>
2025-02-07 19:30:05 +0000

    feat: refactor accordion to details and summary

--- modules/localgov_subsites_paragraphs/js/localgov-accordion.js
+++ modules/localgov_subsites_paragraphs/js/localgov-accordion.js
@@ -27,7 +27,7 @@
        * @param {number} index
        *   Accordion element index.
        */
-      init: function init(accordion, index) {
+      init: function init(accordion) {
         const accordionPanes = accordion.querySelectorAll('.accordion-pane');
         const numberOfPanes = accordionPanes.length;
         const displayShowHide = accordion.hasAttribute(
@@ -35,16 +35,6 @@
         );
         let showHideButton;
         let showHideButtonLabel;
-  
-        for (let i = 0; i < numberOfPanes; i++) {
-          if (displayShowHide) {
-            showHideButton = accordion.querySelector('.accordion-toggle-all');
-            showHideButton.hidden = false;
-            showHideButton.addEventListener('click', toggleAll);
-            showHideButtonLabel =
-              showHideButton.querySelector('.accordion-text');
-          }
-        }
   
         /**
          * Toggles all accordion panes open or closed.
@@ -59,11 +49,22 @@
             showHideButton.dataset[nextState ? 'hideAll' : 'showAll'];
           showHideButton.setAttribute('aria-expanded', nextState);
   
-          const tabopen = nextState ? 'details:not([open]) summary' : 'details[open] summary';
+          const tabopen = nextState
+            ? 'details:not([open]) summary'
+            : 'details[open] summary';
   
           for (let i = 0; i < numberOfPanes; i++) {
-            const accordion = accordionPanes[i].querySelector(tabopen);
-            accordion?.click();
+            const accordionSummary = accordionPanes[i].querySelector(tabopen);
+            accordionSummary?.click();
+          }
+        }
+  
+        for (let i = 0; i < numberOfPanes; i++) {
+          if (displayShowHide) {
+            showHideButton = accordion.querySelector('.accordion-toggle-all');
+            showHideButton.hidden = false;
+            showHideButton.addEventListener('click', toggleAll);
+            showHideButtonLabel = showHideButton.querySelector('.accordion-text');
           }
         }
       },
